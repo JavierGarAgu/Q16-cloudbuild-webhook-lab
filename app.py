14d7c2f7bbba3e5e6ef191a869d5e85bfaf2e3fc
Javier García Aguilera <106744885+JavierGarAgu@users.noreply.github.com>
2026-07-13 11:35:17 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "Binary Authorization Lab - Cloud Build"
+
+@app.get("/health")
+def health():
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
